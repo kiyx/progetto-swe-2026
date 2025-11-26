98e82263e3ca955c84acd3ef3535a0adf6737e47
virginiaesposito <espo.vir.a@gmail.com>
2025-11-26 19:00:16 +0100

    Refactor model classes to use @Getter and @Setter
    Replaced Lombok's @Data annotation with explicit @Getter and @Setter annotations in Issue and Team model classes. This change provides more granular control over generated methods and improves code clarity.

--- backend/src/main/java/dev/parthenodevs/bugboard/backend/model/Issue.java
+++ backend/src/main/java/dev/parthenodevs/bugboard/backend/model/Issue.java
@@ -3,46 +3,66 @@ import dev.parthenodevs.bugboard.backend.model.enums.StatoIssue;
 import dev.parthenodevs.bugboard.backend.model.enums.TipoIssue;
 import dev.parthenodevs.bugboard.backend.model.enums.TipoPriorita;
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+
 @Entity
 public class Issue
 {
     @Column(name = "IdIssue", nullable = false, unique = true)
     @Id
+    @Getter
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idIssue;
 
     @Column(name = "Titolo", length = 200, nullable = false)
+    @Getter
+    @Setter
     private String titolo;
 
     @Column(name = "Descrizione", nullable = false)
+    @Getter
+    @Setter
     private String descrizione;
 
     @Column(name = "Tipo", nullable = false )
+    @Getter
+    @Setter
     @Enumerated(EnumType.ORDINAL)
     private TipoIssue tipo;
 
     @Column(name = "Stato", nullable = false )
+    @Getter
+    @Setter
     @Enumerated(EnumType.ORDINAL)
     private StatoIssue stato = StatoIssue.TODO;
 
     @Column(name = "IsArchiviato" )
+    @Getter
+    @Setter
     private boolean isArchiviato = false;
 
     @Column(name = "Priorita" )
+    @Getter
+    @Setter
     @Enumerated(EnumType.ORDINAL)
     private TipoPriorita priorita;
 
     @Column(name = "Immagine" )
+    @Getter
+    @Setter
     private String immagine;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @Column(name = "IdUtente" )
+    @Getter
+    @Setter
     private Utente utente;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @Column(name = "IdProgetto" )
+    @Getter
+    @Setter
     private Progetto progetto;
 }
--- backend/src/main/java/dev/parthenodevs/bugboard/backend/model/Team.java
+++ backend/src/main/java/dev/parthenodevs/bugboard/backend/model/Team.java
@@ -2,6 +2,8 @@ package dev.parthenodevs.bugboard.backend.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
 @Entity
@@ -10,12 +12,17 @@ public class Team
     @Column(name = "IdTeam", nullable = false, unique = true)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
     private Long idTeam;
 
     @Column(name = "Nome", length = 100, nullable = false)
+    @Getter
+    @Setter
     private String nome;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @Column(name = "IdAdmin" )
+    @Getter
+    @Setter
     private Utente admin;
 }

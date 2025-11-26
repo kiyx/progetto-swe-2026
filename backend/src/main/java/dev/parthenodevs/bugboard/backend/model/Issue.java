eddebe44262d37c478ef5f5d140cacb501138375
virginiaesposito <espo.vir.a@gmail.com>
2025-11-26 18:37:50 +0100

    Add JPA entity annotations to Issue and Team models
    Annotated the Issue and Team classes with JPA and Lombok annotations to define them as entities. Added fields with appropriate column mappings, relationships, and enum handling to support persistence in the database.

--- backend/src/main/java/dev/parthenodevs/bugboard/backend/model/Issue.java
+++ backend/src/main/java/dev/parthenodevs/bugboard/backend/model/Issue.java
@@ -1,6 +1,48 @@
 package dev.parthenodevs.bugboard.backend.model;
+import dev.parthenodevs.bugboard.backend.model.enums.StatoIssue;
+import dev.parthenodevs.bugboard.backend.model.enums.TipoIssue;
+import dev.parthenodevs.bugboard.backend.model.enums.TipoPriorita;
+import jakarta.persistence.*;
+import lombok.Data;
 
+@Data
+@Entity
 public class Issue
 {
+    @Column(name = "IdIssue", nullable = false, unique = true)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idIssue;
 
+    @Column(name = "Titolo", length = 200, nullable = false)
+    private String titolo;
+
+    @Column(name = "Descrizione", nullable = false)
+    private String descrizione;
+
+    @Column(name = "Tipo", nullable = false )
+    @Enumerated(EnumType.ORDINAL)
+    private TipoIssue tipo;
+
+    @Column(name = "Stato", nullable = false )
+    @Enumerated(EnumType.ORDINAL)
+    private StatoIssue stato = StatoIssue.TODO;
+
+    @Column(name = "IsArchiviato" )
+    private boolean isArchiviato = false;
+
+    @Column(name = "Priorita" )
+    @Enumerated(EnumType.ORDINAL)
+    private TipoPriorita priorita;
+
+    @Column(name = "Immagine" )
+    private String immagine;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "IdUtente" )
+    private Utente utente;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "IdProgetto" )
+    private Progetto progetto;
 }
--- backend/src/main/java/dev/parthenodevs/bugboard/backend/model/Team.java
+++ backend/src/main/java/dev/parthenodevs/bugboard/backend/model/Team.java
@@ -1,5 +1,21 @@
 package dev.parthenodevs.bugboard.backend.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
 public class Team
 {
+    @Column(name = "IdTeam", nullable = false, unique = true)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idTeam;
+
+    @Column(name = "Nome", length = 100, nullable = false)
+    private String nome;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "IdAdmin" )
+    private Utente admin;
 }

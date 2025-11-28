53d5afccd8b016601a1ab4c616b4ba03336a67e2
virginiaesposito <espo.vir.a@gmail.com>
2025-11-28 12:07:02 +0100

    Add custom query methods to Issue and Team repositories
    Introduced additional finder methods in IssueRepository and TeamRepository to support more specific queries, such as finding issues by project and type, and teams by admin, members, or associated projects. These changes enhance the flexibility and efficiency of data retrieval for issues and teams.
    
    Co-Authored-By: Giuseppe Paolo Esposito <giuseppep.esposito@studenti.unina.it>

--- backend/src/main/java/dev/parthenodevs/bugboard/backend/repository/TeamRepository.java
+++ backend/src/main/java/dev/parthenodevs/bugboard/backend/repository/TeamRepository.java
@@ -1,10 +1,16 @@
 package dev.parthenodevs.bugboard.backend.repository;
 
+import dev.parthenodevs.bugboard.backend.model.Progetto;
 import dev.parthenodevs.bugboard.backend.model.Team;
+import dev.parthenodevs.bugboard.backend.model.Utente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.*;
 
 public interface TeamRepository extends JpaRepository<Team, Long>
 {
-
+    Optional<Team> findById(Long id);
+    List<Team> findByAdmin(Utente admin);
+    List<Team> findByMembriWithin(Set<Utente> membri);
+    List<Team> findByMembriContains(Utente utente);
+    List<Team> findByProgettiContains(Progetto progetto);
 }

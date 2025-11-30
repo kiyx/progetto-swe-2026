e8d61ae5efd16e74e2b0ade616d7e6b60198a025
virginiaesposito <espo.vir.a@gmail.com>
2025-11-30 15:49:31 +0100

    Add mappers for Issue and Team entities
    Implemented IssueMapper and TeamMapper methods to convert between entity and DTO representations. These changes facilitate mapping Issue and Team objects to their respective response DTOs and creating entities from request DTOs, improving data handling in the backend.
    
    Co-Authored-By: Giuseppe Paolo Esposito <giuseppep.esposito@studenti.unina.it>

--- backend/src/main/java/dev/parthenodevs/bugboard/backend/mapper/IssueMapper.java
+++ backend/src/main/java/dev/parthenodevs/bugboard/backend/mapper/IssueMapper.java
@@ -1,9 +1,52 @@
 package dev.parthenodevs.bugboard.backend.mapper;
 
+import dev.parthenodevs.bugboard.backend.dto.request.CreateIssueRequestDTO;
+import dev.parthenodevs.bugboard.backend.dto.request.CreateTeamRequestDTO;
+import dev.parthenodevs.bugboard.backend.dto.response.IssueResponseDTO;
+import dev.parthenodevs.bugboard.backend.dto.response.TeamResponseDTO;
+import dev.parthenodevs.bugboard.backend.model.Issue;
+import dev.parthenodevs.bugboard.backend.model.Progetto;
+import dev.parthenodevs.bugboard.backend.model.Team;
+import dev.parthenodevs.bugboard.backend.model.Utente;
 import org.springframework.stereotype.Component;
 
 @Component
 public class IssueMapper
 {
+    public IssueResponseDTO toDto(Issue issue)
+    {
+        if(issue == null)
+            return null;
+
+        return IssueResponseDTO.builder()
+                .id(issue.getId())
+                .stato(issue.getStato())
+                .descrizione(issue.getDescrizione())
+                .idAutore(issue.getAutore().getId())
+                .idProgetto(issue.getProgetto().getId())
+                .tipo(issue.getTipo())
+                .isArchiviato(issue.isArchiviato())
+                .immagine(issue.getImmagine())
+                .titolo(issue.getTitolo())
+                .priorita(issue.getPriorita())
+                .build();
+
+    }
+
+    public Issue toEntity(CreateIssueRequestDTO request, Utente autore, Progetto progetto)
+    {
+        if(request == null)
+            return null;
+
+        return Issue.builder()
+                .autore(autore)
+                .descrizione(request.getDescrizione())
+                .immagine(request.getImmagine())
+                .priorita(request.getPriorita())
+                .progetto(progetto)
+                .tipo(request.getTipo())
+                .titolo(request.getTitolo())
+                .build();
+    }
 
 }
--- backend/src/main/java/dev/parthenodevs/bugboard/backend/mapper/TeamMapper.java
+++ backend/src/main/java/dev/parthenodevs/bugboard/backend/mapper/TeamMapper.java
@@ -1,9 +1,46 @@
 package dev.parthenodevs.bugboard.backend.mapper;
 
+import dev.parthenodevs.bugboard.backend.dto.request.CreateTeamRequestDTO;
+import dev.parthenodevs.bugboard.backend.dto.request.RegisterRequestDTO;
+import dev.parthenodevs.bugboard.backend.dto.response.TeamResponseDTO;
+import dev.parthenodevs.bugboard.backend.dto.response.UtenteResponseDTO;
+import dev.parthenodevs.bugboard.backend.exception.InvalidFieldException;
+import dev.parthenodevs.bugboard.backend.model.Progetto;
+import dev.parthenodevs.bugboard.backend.model.Team;
+import dev.parthenodevs.bugboard.backend.model.Utente;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Component
 public class TeamMapper
 {
+    public TeamResponseDTO toDto(Team team)
+    {
+        if(team == null)
+            return null;
+
+        return TeamResponseDTO.builder()
+                .id(team.getId())
+                .nome(team.getNome())
+                .nomeAdmin(team.getAdmin().getNome())
+                .build();
+    }
+
+    public Team toEntity(CreateTeamRequestDTO request, Utente admin)
+    {
+        if(request == null)
+            return null;
+
+        return Team.builder()
+                .nome(request.getNome())
+                .admin(admin)
+                .build();
+    }
 
 }

1c525f6e1ae1fa4abd622fc37acacf334be6945f
kiyx <espositogiuseppe2003@outlook.it>
2026-01-03 17:00:14 +0100

    Add design document, update image, add IssueServiceTest
    Added the INGSW2526_005_design.pdf design document and updated the Contributor_Commits.png image. Introduced an initial IssueServiceTest class with Mockito extension setup.

---
+++ backend/src/test/java/dev/parthenodevs/bugboard/backend/service/IssueServiceTest.java
@@ -0,0 +1,10 @@
+package dev.parthenodevs.bugboard.backend.service;
+
+import org.junit.jupiter.api.extension.*;
+import org.mockito.junit.jupiter.*;
+
+@ExtendWith(MockitoExtension.class)
+public class IssueServiceTest
+{
+
+}
